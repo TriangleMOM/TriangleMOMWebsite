30515802c16547fffffcc675a1c02cdcd09d3090
JSpencerPittman <jspencerpittman@gmail.com>
2024-01-03 14:33:18 -0600

    Mobile support for eboard page

--- trianglemom/src/pages/eboard/Eboard.js
+++ trianglemom/src/pages/eboard/Eboard.js
@@ -3,16 +3,16 @@ import Header from './../../components/header/Header.js';
 
 function Profile() {
     return (
-        <div class="profile-container">
+        <div class="profile">
             <div class='profile-image'>
                 <img src="./images/profilepic.jpg"/>
             </div>
             <div class='profile-info'>
-                <h2>Jason Pittman</h2>
-                <h3>Recruitment Chair</h3>
-                <p class='hometown'>My hometown is Grain Valley, Missouri</p>
-                <p class='major'>Majoring in Computer Science & Mathematics</p>
-                <p class='quote'>"I joined triangle to become part of a family where I'm dad"</p>
+                <h2 class="profile-name">Jason Pittman</h2>
+                <h3 class="profile-position">Recruitment Chair</h3>
+                <p class='profile-hometown'>My hometown is Grain Valley, Missouri</p>
+                <p class='profile-major'>Majoring in Computer Science & Mathematics</p>
+                <p class='profile-quote'>"I joined triangle to become part of a family where I'm dad"</p>
             </div>
         </div>
     );
