1df6286aa1615661412fa8ae3841cf8c0fbe222c
JSpencerPittman <jspencerpittman@gmail.com>
2024-01-05 12:50:57 -0600

    Temporary commit

--- trianglemom/src/App.js
+++ trianglemom/src/App.js
@@ -1,7 +1,7 @@
 import SideBar from './components/sidebar/Sidebar.js'
 import './App.css'
 import Connect from './pages/connect/Connect.js';
-import Gallery from './pages/gallery/Gallery.js';
+import Album from './pages/album/Album.js';
 import Eboard from './pages/eboard/Eboard.js';
 import Home from './pages/home/Home.js'
 import Events from './pages/events/Events.js';
@@ -9,10 +9,10 @@ import Events from './pages/events/Events.js';
 import { useState, useEffect } from 'react';
 import Navbar from './components/navbar/Navbar.js';
 
-const MOBILE_WIDTH = 1000;
+const WIDTH_THRESHOLD = 1000;
 
 function App() {
-  const [activePage, setActivePage] = useState("connect");
+  const [activePage, setActivePage] = useState("album");
   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
 
   const handleActivePageChange = (pageName) => {
@@ -31,16 +31,18 @@ function App() {
     };
   }, []);
 
+  const narrowLayout = windowWidth < WIDTH_THRESHOLD;
+
   return (
     <div id="body">
-      {windowWidth < MOBILE_WIDTH && <Navbar />}
-      {windowWidth >= MOBILE_WIDTH && <SideBar activePage={activePage} handleActivePageChange={handleActivePageChange} />}
+      {narrowLayout && <Navbar />}
+      {!narrowLayout && <SideBar activePage={activePage} handleActivePageChange={handleActivePageChange} />}
       <div id="content-container">
-        {/* {activePage === 'home' && <Home/>}
-        {activePage === 'connect' && <Connect/>}
-        {activePage === 'events' && <Events/>}
-        {activePage === 'eboard' && <Eboard/>}
-        {activePage === 'gallery' && <Gallery/>} */}
+        {/* {activePage === 'home' && <Home/>} */}
+        {/* {activePage === 'connect' && <Connect/>} */}
+        {/* {activePage === 'events' && <Events/>} */}
+        {/* {activePage === 'eboard' && <Eboard/>} */}
+        {activePage === 'album' && <Album/>}
       </div>
     </div>
   );
